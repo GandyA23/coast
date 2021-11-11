a8c05e1763e75285ba7c0467f0da70a825288ab7
Gustavo Lopez <thered04@outlook.com>
2021-11-11 11:28:33 -0600

    correccion de pantalla de registro, cambio de estilos al archivo style.gl.scss

--- src/pages/MetodoPago.vue
+++ src/pages/MetodoPago.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="row">
-    <div class="q-pa-md col-md-12 register">
-      <h4 class="text-weight-bold cf-title cc-pri" style="margin-left:10px">pago</h4>
-      <q-input rounded outlined v-model="name" bg-color="white" dense>
-        <p class="title-input">Nombre:</p>
+    <div class="q-pa-md col-md-12 cf-body register">
+      <h4 class="text-weight-bold cf-title cc-pri">
+        pago
+      </h4>
+      <q-input
+        rounded
+        outlined
+        v-model="name"
+        :rules="[
+          (val) => !!val || 'Este campo es requerido',
+          (val) => val.length <= 20 || 'Por favor máximo 20 caracteres',
+          (val) => val.length >= 5 || 'Por favor mínimo 5 caracteres',
+        ]"
+        dense
+      >
+        <p class="title-input text-grey-6">Nombre:</p>
         <template v-slot:prepend>
-          <q-icon name="assignment_ind" class="color-icon" />
+          <q-icon name="assignment_ind" class="cc-pri" />
         </template>
       </q-input>
 
@@ -13,48 +25,75 @@
         rounded
         outlined
         v-model="lastName"
-        style="padding-top: 50px"
-        bg-color="white"
+        class="last-name"
+        :rules="[
+          (val) => !!val || 'Este campo es requerido',
+          (val) => val.length <= 20 || 'Por favor máximo 20 caracteres',
+          (val) => val.length >= 5 || 'Por favor mínimo 5 caracteres',
+        ]"
         dense
       >
-        <p class="title-input">Apellido:</p>
+        <p class="title-input text-grey-6">Apellido:</p>
         <template v-slot:prepend>
-          <q-icon name="assignment_ind" class="color-icon" />
+          <q-icon name="assignment_ind" class="cc-pri" />
         </template>
       </q-input>
-
+      <q-img
+        class="img-visa"
+        src="https://i.ibb.co/WP7MTyg/visa-mastercard.png"
+      ></q-img>
       <q-input
         rounded
         outlined
         v-model="creditCard"
-        style="padding-top: 50px"
-        bg-color="white"
+        class="credit-card"
+        mask="####-####-####-####"
+        :rules="[
+          (val) => !!val || 'Este campo es requerido',
+          (val) => val.length == 19 || 'Ingrese un numero de tarjeta valido',
+        ]"
         dense
       >
-        <q-img
-          style="max-width: 100px; position absoulte; top: -40px"
-          src="https://i.ibb.co/WP7MTyg/visa-mastercard.png"
-        ></q-img>
-        <p class="title-input">Numero de tarjeta:</p>
+        <p class="title-input text-grey-6">Numero de tarjeta:</p>
 
         <template v-slot:prepend>
-          <q-icon name="email" class="color-icon" />
+          <q-icon name="email" class="cc-pri"/>
         </template>
       </q-input>
 
       <div class="date-cvv">
         <q-input
+          v-model="date"
           rounded
           outlined
-          v-model="date"
-          style="padding-top: 20px; width: 55%"
-          bg-color="white"
-          clear-icon="native_date"
           dense
+          mask="##-##-####"
+          class="date-input"
+          :rules="[
+            (val) => !!val || 'Este campo es requerido',
+            (val) => val.length == 10 || 'Ingrese una fecha valida',
+            validateDate,
+          ]"
         >
-          <p class="title-input" style="width: 200px">Fecha de vencimiento:</p>
+          <p class="title-input text-grey-6">
+            Fecha de vencimiento:
+          </p>
           <template v-slot:prepend>
-            <q-icon name="date_range" class="color-icon" />
+            <q-icon name="event" class="cursor-pointer cc-pri">
+              <q-popup-proxy transition-show="scale" transition-hide="scale">
+                <q-date
+                  v-model="date"
+                  :locale="myLocale"
+                  navigation-min-year-month="2021/01"
+                  navigation-max-year-month="2026/12"
+                  mask="MM-DD-YYYY"
+                >
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Aceptar" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
           </template>
         </q-input>
 
@@ -62,27 +101,34 @@
           rounded
           outlined
           v-model="cvv"
-          style="padding-top: 20px; width: 40%"
-          bg-color="white"
+          class="cvv-input"
+          mask="###"
+          :rules="[
+            (val) => !!val || 'Este campo es requerido',
+            (val) => val.length == 3 || 'Ingrese un numero de CVV/CVC valido',
+          ]"
           dense
         >
-          <p class="title-input">CVV/CVC:</p>
+          <p class="title-input text-grey-6">CVV/CVC:</p>
           <template v-slot:prepend>
-            <q-icon name="credit_card" class="color-icon" />
+            <q-icon name="credit_card" class="cc-pri" />
           </template>
         </q-input>
       </div>
 
       <q-btn
         rounded
-        class="full-width"
-        style="margin-top: 20px; background-color: #145da0; color: white"
-        text-color="#0C2D48"
+        class="full-width btn-register"
         label="Guardar tarjeta"
+        color="cc-pri"
       />
     </div>
-    <div class="q-pa-sm col-sm-4 img" >
-      <q-img src="https://i.ibb.co/KxyvC9Q/tarjeta.png" style="margin-top:40px"> </q-img>
+    <div class="q-pa-sm col-sm-4 img">
+      <q-img
+        src="https://i.ibb.co/KxyvC9Q/tarjeta.png"
+        style="margin-top: 40px"
+      >
+      </q-img>
     </div>
   </div>
 </template>
@@ -96,52 +142,42 @@ export default {
       name: "",
       lastName: "",
       creditCard: "",
-      date: "",
+      date: "01-01-2021",
       cvv: "",
+
+      myLocale: {
+        days: "Domingo_Lunes_Martes_Miércoles_Jueves_Viernes_Sábado".split("_"),
+        daysShort: "Dom_Lun_Mar_Mié_Jue_Vie_Sáb".split("_"),
+        months:
+          "Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre".split(
+            "_"
+          ),
+        monthsShort: "Ene_Feb_Mar_Abr_May_Jun_Jul_Ago_Sep_Oct_Nov_Dic".split(
+          "_"
+        ),
+        firstDayOfWeek: 1,
+      },
     };
+  },
+
+  methods: {
+    validateDate() {
+      let date = this.date.split("-");
+      if (
+        date[0] < "01" ||
+        date[0] > "12" ||
+        date[1] < "01" ||
+        date[1] > "31" ||
+        date[2] < "2021" ||
+        date[2] > "2026"
+      ) {
+        return "Ingrese una fecha valida";
+      }
+    },
   },
 };
 </script>
 
 <style lang="sass" scoped>
-
-.title-input
-  position: absolute
-  color: black
-  top: -20px
-  left: -30px
-
-.date-cvv
-  display: flex
-  justify-content: space-between
-  padding-top: 30px
-
-.color-icon
-  color: #145DA0
-
-.title
-  display: flex
-  justify-content: center
-  color: white
-  margin: 0
-  margin-bottom: 15px
-
-.btn-register
-  display: flex
-  justify-content: center
-  color: white
-  margin-top: 20px
-  color: #0C2D48
-
-.register
-  width: 100%
-  max-width: 500px
-  border-radius: 30px
-  margin: auto
-  margin-top: -30px
-
-.img
-  width: 100%
-  margin: auto
-  max-width: 450px
+@import "../css/style.gl.scss"
 </style>

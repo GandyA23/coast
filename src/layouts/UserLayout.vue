7cc881030adf490a90c0a6eaf1cd00ab7f97fcef
GandyA23 <avila.gandy@gmail.com>
2021-12-13 04:48:38 -0600

    Se agrego el InboxEmail al esqueleto del usuario

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -107,6 +107,15 @@ const linksData = [
         classes: 'cc-pri cf-body text-body2'
       }
     ]
+  },
+  {
+    title: 'Inbox Email',
+    icon: {
+      name: 'o_mail',
+      classes: 'cc-pri'
+    },
+    link: '',
+    classes: 'cc-pri cf-body text-weight-bolder'
   }
 ];
 
